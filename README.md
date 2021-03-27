4304e2a21138cbd11dce7d1fe18023c918a44030
Kartik Grewal <kartik.grewal@gmail.com>
2021-03-27 11:09:46 +0530

    feat(template): sample-auth template (#3)
    Added authentication integration with React.js template

--- README.md
+++ README.md
@@ -24,4 +24,14 @@ Sample iOS Application built with Swift for getting a party started.
 
 ![Screenshot](./Canolo-iOS/screenshots/IMG_0264.PNG)
 
+---
+
+<br/>
+
+### 🔐 [Google Signin with React using Graphql](./google-signin-with-react/)
+
+Sample web app built using React, demonstrating how google signin works with Canonic via graphql APIs
+
+![Screenshot](./google-signin-with-react/screenshots/screenshot.gif)
+
 > ### More coming soon 🚀
---
+++ google-signin-with-react/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://moviehunt-copy.hem.canonic.dev/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
