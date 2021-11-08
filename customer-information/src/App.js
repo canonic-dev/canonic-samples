4e96bb90e3e64ec549b5f8884927027a4d1131ff
Simranjot <iamsimranjot@gmail.com>
2021-11-09 03:45:59 +0530

    adds table and header

--- customer-information/src/App.js
+++ customer-information/src/App.js
@@ -1,9 +1,22 @@
+import InformationTable from "./components/Information Table/InformationTable";
+import { Container, Navbar } from "react-bootstrap";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Navbar bg="secondary" variant="dark" className="mb-5">
+        <Container>
+          <Navbar.Brand href="#home">Customer Information</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <InformationTable></InformationTable>
+      </Container>
+      <Container className="mt-5">
+        <div>Canonic ©2021 Created by Canonic Inc</div>
+      </Container>
     </div>
   );
 }
---
+++ customer-information/src/components/Information Table/InformationTable.js
@@ -0,0 +1,40 @@
+import Table from "react-bootstrap/Table";
+
+const InformationTable = () => {
+  return (
+    <div class="table-responsive">
+      <Table class="table table-hove">
+        <thead class="table-light">
+          <tr>
+            <th>#</th>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <th key={index}>Table heading</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>2</td>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>3</td>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default InformationTable;
