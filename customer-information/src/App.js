92d533cb4d69b36724b6eff2c6f6f906af170bfa
Simranjot <iamsimranjot@gmail.com>
2021-11-17 00:48:48 +0530

    adds backend integratio

--- customer-information/src/App.js
+++ customer-information/src/App.js
@@ -1,23 +1,38 @@
-import InformationTable from "./components/Information Table/InformationTable";
+import React from "react";
 import { Container, Navbar } from "react-bootstrap";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import InformationTable from "./components/Information Table/InformationTable";
 
 import "./App.css";
 
+/**
+ * Connecting with the backend using apollo client
+ */
+const client = new ApolloClient({
+  // make sure you update the URI here to point to your backend
+  uri: "https://stripe-integration.hem.staging.canonic.dev/graphql",
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+});
+
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="secondary" variant="dark" className="mb-5">
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Navbar bg="secondary" variant="dark" className="mb-5">
+          <Container>
+            <Navbar.Brand href="#home">Customer Information</Navbar.Brand>
+          </Container>
+        </Navbar>
         <Container>
-          <Navbar.Brand href="#home">Customer Information</Navbar.Brand>
+          <InformationTable></InformationTable>
         </Container>
-      </Navbar>
-      <Container>
-        <InformationTable></InformationTable>
-      </Container>
-      <Container className="mt-5">
-        <div>Canonic ©2021 Created by Canonic Inc</div>
-      </Container>
-    </div>
+        <Container className="mt-5">
+          <div>Canonic ©2021 Created by Canonic Inc</div>
+        </Container>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- customer-information/src/components/Information Table/InformationTable.js
+++ customer-information/src/components/Information Table/InformationTable.js
@@ -1,119 +1,43 @@
-import Table from "react-bootstrap/Table";
-
-const dummyData = [
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-  {
-    Name: "Simranjot",
-    Email: "simranjot@canonic.dev",
-    "Current Plan": "Enterprise",
-    "Subscription Type": "Yearly",
-    "Subscription Cycle": "2021-2022",
-    "Total Purchase": "$1250",
-  },
-];
+import React from "react";
+import { Table, Spinner } from "react-bootstrap";
+import { GET_CUSTOMERS_INFO } from "../../gql/queries";
+import { useQuery } from "@apollo/client";
 
 const InformationTable = () => {
+  const { data = {}, loading } = useQuery(GET_CUSTOMERS_INFO);
+  const { customerInfos = [] } = data;
+  const { customers = {} } = customerInfos[0] ?? {};
+  const customerData = customers.data ?? [];
+
+  if (loading)
+    return (
+      <div class="d-flex justify-content-center">
+        <Spinner animation="border" />
+      </div>
+    );
+
   return (
     <div class="table-responsive">
       <Table class="table table-hove">
         <thead class="table-light">
           <tr>
             <th>#</th>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <th key={index}>Table heading</th>
-            ))}
+            {Object.keys(customerData[0] ?? {}).map((keyName, index) => {
+              return <th key={index}>{keyName.toUpperCase()}</th>;
+            })}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <td key={index}>Table cell {index}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>2</td>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <td key={index}>Table cell {index}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>3</td>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <td key={index}>Table cell {index}</td>
-            ))}
-          </tr>
+          {customerData.map((customer, index) => {
+            return (
+              <tr>
+                <td>{index}</td>
+                {Object.keys(customer).map((keyName, index) => {
+                  return <td key={index}>{customer[keyName] ?? "N/A"}</td>;
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
@@ -121,3 +45,86 @@ const InformationTable = () => {
 };
 
 export default InformationTable;
+
+// const dummyData = [
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+//   {
+//     Name: "Simranjot",
+//     Email: "simranjot@canonic.dev",
+//     "Current Plan": "Enterprise",
+//     "Subscription Type": "Yearly",
+//     "Subscription Cycle": "2021-2022",
+//     "Total Purchase": "$1250",
+//   },
+// ];
